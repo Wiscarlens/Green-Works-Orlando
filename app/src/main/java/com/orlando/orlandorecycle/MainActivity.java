332aebaef08464d4219eaa06ab27d1b15be5d8f8
Wiscarlens Lucius <wiscarlens@gmail.com>
2024-01-28 10:13:43 -0500

    Add team members

--- app/src/main/java/com/orlando/orlandorecycle/MainActivity.java
+++ app/src/main/java/com/orlando/orlandorecycle/MainActivity.java
@@ -1,5 +1,15 @@
 package com.orlando.orlandorecycle;
 
+/*
+* This is a collaborative effort by the following team members:
+ * Team members:
+ * - Wiscarlens Lucius (Team Leader)
+ * - Amanpreet Singh
+ * - Alexandra Perez
+ * - Eric Klausner
+ * - Jordan kinlocke
+ * */
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;

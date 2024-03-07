0857c91ae6107c7eda98c83c6401322f6dde8fee
sneseric <eklausner@mail.valenciacollege.edu>
2024-03-06 20:16:42 -0500

    update calendar using recollect api, styling changes to maps page

--- app/src/main/java/com/orlando/greenworks/CalendarFragment.java
+++ app/src/main/java/com/orlando/greenworks/CalendarFragment.java
@@ -11,36 +11,57 @@ package com.orlando.greenworks;
  * */
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.Toast;
-
-import com.orlando.greenworks.R;
-
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.webkit.CookieManager;
+import android.view.MotionEvent;
 
 public class CalendarFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    private WebView webView;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-
-        CalendarView calendarView = view.findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                // Handle the date change
-                String msg = "Selected date: " + dayOfMonth + "/" + (month + 1) + "/" + year;
-                Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        webView = view.findViewById(R.id.webview);
+        initWebView();
+        loadHtmlContentFromUrl();
         return view;
+    }
+
+    private void initWebView() {
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        }
+        // Enable hardware acceleration
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        // Enable built-in zoom mechanisms and initialize to not allow zooming
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+        // Enable scrolling
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+
+    }
+
+    private void loadHtmlContentFromUrl() {
+        String url = "https://greenworkscalendar.netlify.app";
+        //String url = "file:///android_asset/calendar.html";
+        webView.loadUrl(url);
     }
 }

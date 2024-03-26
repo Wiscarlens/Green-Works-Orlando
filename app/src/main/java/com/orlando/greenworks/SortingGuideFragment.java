c18cfc6279e73994ae2bc474bef6d83294b794a7
sneseric <eklausner@mail.valenciacollege.edu>
2024-03-25 23:25:48 -0400

    added click listener for close button on sorting guide. Also fixed issue where user cannot see item information layout after entering search query in sorting guide search.

--- app/src/main/java/com/orlando/greenworks/SortingGuideFragment.java
+++ app/src/main/java/com/orlando/greenworks/SortingGuideFragment.java
@@ -1,53 +1,79 @@
 package com.orlando.greenworks;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-
+import android.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-
 import java.util.Objects;
+import android.widget.ImageButton;
 
-/*
- * This is a collaborative effort by the following team members:
- * Team members:
- * - Wiscarlens Lucius (Team Leader)
- * - Amanpreet Singh
- * - Alexandra Perez
- * - Eric Klausner
- * - Jordan Kinlocke
- * */
 
 public class SortingGuideFragment extends BottomSheetDialogFragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-    }
-
+    //TODO: Add real data to "Most Frequent"
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sorting_guide, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_sorting_guide, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        SearchView searchView = view.findViewById(R.id.search);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("SortingGuideFragment", "Search query submitted: " + query);
 
+                // Create a new instance of ItemInformationFragment
+                ItemInformationFragment itemInformationFragment = new ItemInformationFragment();
+
+                // Bundle to pass the search query to ItemInformationFragment
+                Bundle args = new Bundle();
+                args.putString("searchQuery", query); // Use the same key when retrieving the value in ItemInformationFragment
+                itemInformationFragment.setArguments(args);
+
+                // Replace SortingGuideFragment with ItemInformationFragment in the fragment container
+                if (getFragmentManager() != null) {
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_container, itemInformationFragment);
+                    transaction.addToBackStack(null); // Optional, allows users to navigate back to the previous fragment
+                    transaction.commit();
+                }
+
+                // Dismiss the SortingGuideFragment when user enters search query. This will
+                // remove the bottom sheet from the screen so the user can see item information screen.
+                // Added since bottom sheet layout is now fullscreen and search query results were not obvious to user.
+                dismiss();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        // Make close button work
         ImageButton closeButton = view.findViewById(R.id.closeButton);
-        closeButton.setOnClickListener(v -> dismiss());
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss(); // This will dismiss the BottomSheetDialogFragment
+            }
+        });
+
+        return view;
+
     }
+
 
     @Override
     public void onStart() {
@@ -70,6 +96,5 @@ public class SortingGuideFragment extends BottomSheetDialogFragment {
             }
 
             Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
-        }
-    }
-}
+
+}}}

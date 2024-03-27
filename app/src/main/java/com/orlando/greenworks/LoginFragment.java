ade338eb52b1d16a07233836c69f0d8047a98fde
Amanpreet Singh <51552560+Singhamanpreet2002@users.noreply.github.com>
2024-03-26 20:32:51 -0400

    Login / registration
    using local db

--- app/src/main/java/com/orlando/greenworks/LoginFragment.java
+++ app/src/main/java/com/orlando/greenworks/LoginFragment.java
@@ -10,6 +10,7 @@ package com.orlando.greenworks;
  * - Jordan Kinlocke
  * */
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -22,10 +23,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
+
+    EditText logEmail, logPassword;
+    Button loginButton;
+    LocalDataStorage localDataStorage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,6 +62,38 @@ public class LoginFragment extends Fragment {
             changeFragment(new HomeFragment());
         });
 
+        logEmail = logEmail.findViewById(R.id.logEmail);
+        logPassword = logPassword.findViewById(R.id.logPassword);
+        loginButton = loginButton.findViewById(R.id.loginButton);
+
+        localDataStorage = new LocalDataStorage(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginUsername = logEmail.getText().toString().trim();
+                String loginPassword = logPassword.getText().toString().trim();
+
+                // Retrieve user data from local storage
+                String[] userData = localDataStorage.getUserData();
+                String savedEmail = userData[3];
+                String savedPassword = userData[4];
+
+                if (loginUsername.equals(savedEmail) && loginPassword.equals(savedPassword)) {
+                    // Login successful
+//                    Toast.makeText(LoginFragment.this, "Login successful", Toast.LENGTH_SHORT).show();
+//
+//                    Intent intent = new Intent(LoginFragment.this, HomeFragment.class);
+//                    startActivity(intent);
+
+                    // Finish current activity to prevent user from coming back to login screen using back button
+//                    mainActivity.finish();
+                } else {
+                    // Login failed
+//                    Toast.makeText(LoginFragment.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return view;
     }
 

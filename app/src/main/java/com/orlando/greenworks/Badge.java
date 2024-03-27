5bbb3c3da9ac87b963c45d5af54e2a270644462f
Wiscarlens Lucius <wiscarlens@gmail.com>
2024-03-27 15:08:32 -0400

    Create Badge.java class

---
+++ app/src/main/java/com/orlando/greenworks/Badge.java
@@ -0,0 +1,69 @@
+package com.orlando.greenworks;
+
+public class Badge {
+    private String title;
+    private String description;
+    private String imagePath;
+    private int maxPoints;
+    private int points;
+
+    public Badge(String title, String imagePath) {
+        this.title = title;
+        this.imagePath = imagePath;
+    }
+
+    public Badge(String title, String imagePath, int maxPoints, int points) {
+        this.title = title;
+        this.imagePath = imagePath;
+        this.maxPoints = maxPoints;
+        this.points = points;
+    }
+
+    public Badge(String title, String description, String imagePath, int maxPoints, int points) {
+        this.title = title;
+        this.description = description;
+        this.imagePath = imagePath;
+        this.maxPoints = maxPoints;
+        this.points = points;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getMaxPoints() {
+        return maxPoints;
+    }
+
+    public void setMaxPoints(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}

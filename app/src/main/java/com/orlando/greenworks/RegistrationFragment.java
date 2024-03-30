c723dbb491d6ab4486f232bb5442f99e23a6a30e
sneseric <eklausner@mail.valenciacollege.edu>
2024-03-29 22:39:06 -0400

    Fixed registration layout ("Login" "Sign-Up" and "Logout" were crashing the app) since "login" branch merge. Also, multiple instance bug for sorting guide search was reintroduced (pressing "Exit" on Item Information Screen was opening second instance of Sorting Guide). Reverted SortingGuideFragment and IteInformationFragment code to commit bda4a62 to fix this.

--- app/src/main/java/com/orlando/greenworks/RegistrationFragment.java
+++ app/src/main/java/com/orlando/greenworks/RegistrationFragment.java
@@ -1,19 +1,18 @@
 package com.orlando.greenworks;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
-import android.view.View;
-
-import android.widget.Button;
-import android.widget.EditText;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -21,7 +20,6 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.util.Objects;
-
 
 /*
  * This is a collaborative effort by the following team members:
@@ -36,12 +34,10 @@ import java.util.Objects;
 
 public class RegistrationFragment extends BottomSheetDialogFragment {
 
-    EditText regFirstName, regLastName, regAddress, regEmail, regPassword, regConfirmPassword;
-    Button regAccountBtn;
-    LocalDataStorage localDataStorage;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -64,34 +60,6 @@ public class RegistrationFragment extends BottomSheetDialogFragment {
 
 
         // Inflate the layout for this fragment
-
-        regFirstName= regFirstName.findViewById(R.id.regFirstName);
-        regLastName= regLastName.findViewById(R.id.regLastName);
-        regAddress = regAddress.findViewById(R.id.regAddress);
-        regEmail = regEmail.findViewById(R.id.regEmail);
-        regPassword = regPassword.findViewById(R.id.logPassword);
-        regConfirmPassword = regConfirmPassword.findViewById(R.id.regConfirmPassword);
-        localDataStorage = new LocalDataStorage(this);
-
-        regAccountBtn = regAccountBtn.findViewById(R.id.regAccountBtn);
-
-        regAccountBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String firstName = regFirstName.getText().toString().trim();
-                String lastName = regLastName.getText().toString().trim();
-                String address = regAddress.getText().toString().trim();
-                String email = regEmail.getText().toString().trim();
-                String password = regPassword.getText().toString().trim();
-                String confirmPassword = regConfirmPassword.getText().toString().trim();
-
-                localDataStorage.saveUserData(firstName,lastName,address,email,password,confirmPassword);
-
-//                Toast.makeText(new RegistrationFragment(), "Signup successful", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
         return view;
     }
 
@@ -124,9 +92,5 @@ public class RegistrationFragment extends BottomSheetDialogFragment {
 
             Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
         }
-    }
-
-    public SharedPreferences getSharedPreferences(String prefName, int modePrivate) {
-        return null;
     }
 }

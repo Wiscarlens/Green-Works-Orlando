f5c7e6959518e66a0828d273827d7f80f5bd6b05
jkinlocke <kinlocke.jordan@gmail.com>
2024-04-15 23:26:57 -0400

    added a switch for enabling notifications when the user clicks the switch they will see a popup that ells them they have enabled notifications and the opposite if they click it again.

---
+++ app/src/main/java/com/orlando/greenworks/NotificationReceiver.java
@@ -0,0 +1,38 @@
+package com.orlando.greenworks;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    private static final String CHANNEL_ID = "com.orlando.greenworks.notification.channel";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        createNotificationChannel(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo_full)
+                .setContentTitle("Pickup Reminder")
+                .setContentText("Don't forget your pickup tomorrow!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
+    }
+
+    private void createNotificationChannel(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Pickup Reminder";
+            String description = "Channel for pickup reminder";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
